1cc8e42e6345bc434e4e21a82079258ce30e413d
Innocent Mambo <61348138+KakaInnocent@users.noreply.github.com>
2025-04-03 15:17:16 +0300

    py to the rescue

---
+++ Over-The-Wire/Bandit/level5.py
@@ -0,0 +1,10 @@
+from pwn import *
+
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit4'
+password = 'pIwrPrtPN36QITSp3EQaw936yaFoFgAB'
+cmd = 'cat < inhere/-file07'
+
+s =  ssh(host=hostname, user=username, password=password)
+ex = s.run(cmd)
+print ex.recvall()
