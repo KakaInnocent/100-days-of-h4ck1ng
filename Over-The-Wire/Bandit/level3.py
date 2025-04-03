dab23c12e3ce47f0b3a09496d418fe854d24e400
Innocent Mambo <61348138+KakaInnocent@users.noreply.github.com>
2025-04-03 15:15:52 +0300

    challenge 3

---
+++ Over-The-Wire/Bandit/level3.py
@@ -0,0 +1,10 @@
+from pwn import *
+
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit2'
+password = 'CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9'
+cmd = 'cat "spaces in this filename"'
+
+s =  ssh(host=hostname, user=username, password=password)
+ex = s.run(cmd)
+print ex.recvall()
