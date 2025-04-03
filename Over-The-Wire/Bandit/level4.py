e868f9421848aa7ee54d70a0fa1f30270ac4a3f2
Innocent Mambo <61348138+KakaInnocent@users.noreply.github.com>
2025-04-03 15:16:30 +0300

    pwntools for the win

---
+++ Over-The-Wire/Bandit/level4.py
@@ -0,0 +1,10 @@
+from pwn import *
+
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit3'
+password = 'UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK'
+cmd = 'cat inhere/.hidden'
+
+s =  ssh(host=hostname, user=username, password=password)
+ex = s.run(cmd)
+print ex.recvall()
