da7174dfdea4f96fb7e9776ef4e8080673a26ef8
Innocent Mambo <61348138+KakaInnocent@users.noreply.github.com>
2025-04-08 09:15:07 +0300

    l8

---
+++ Over-The-Wire/Bandit/level8.py
@@ -0,0 +1,10 @@
+from pwn import *
+
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit7'
+password = 'HKBPTKQnIay4Fw76bEy8PVxKEDQRKTzs'
+cmd = "grep \"millionth\" data.txt | awk -F'\t' '{print $2}'"
+
+s =  ssh(host=hostname, user=username, password=password)
+ex = s.run(cmd)
+print ex.recvall()
