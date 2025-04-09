81e43b25b9bf03c80b16da563f4a7bd58da8038e
Innocent Mambo <61348138+KakaInnocent@users.noreply.github.com>
2025-04-09 09:21:39 +0300

    lvl12

---
+++ Over-The-Wire/Bandit/level12.py
@@ -0,0 +1,10 @@
+from pwn import *
+
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit11'
+password = 'IFukwKGsFW8MOq3IRFqrxE1hxTNEbUPR'
+cmd = "cat data.txt | tr 'A-Za-z' 'N-ZA-Mn-za-m' | sed -e 's/The password is //'"
+
+s =  ssh(host=hostname, user=username, password=password)
+ex = s.run(cmd)
+print ex.recvall()
