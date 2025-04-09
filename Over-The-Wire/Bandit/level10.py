3fc64c73c4502e5218e9f30e602424c43886e64d
Innocent Mambo <61348138+KakaInnocent@users.noreply.github.com>
2025-04-09 09:20:25 +0300

    l10

---
+++ Over-The-Wire/Bandit/level10.py
@@ -0,0 +1,10 @@
+from pwn import *
+
+hostname = 'bandit.labs.overthewire.org'
+username = 'bandit9'
+password = 'UsvVyFSfZZWbi6wgC7dAFyFuR6jQQUhR'
+cmd = "strings data.txt | grep '==' | awk -F' ' '{print $2}' | awk 'length($0)==32'"
+
+s =  ssh(host=hostname, user=username, password=password)
+ex = s.run(cmd)
+print ex.recvall()
